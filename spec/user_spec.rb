bf2bf2f4a18091a605180ec4fd7eee2adbc9a2b5
Lena Krasnova <olkras03@gmail.com>
2019-11-18 15:44:23 +0000

    unit test for password

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,8 +1,13 @@
-require 'user'
+require "user"
 
 describe User do
-  it 'should create a user' do
+  it "should create a user" do
     User.create("test_username", "test_password", "test@test.com", "01234567890")
     expect(User.get_by_username("test_username")).not_to be_nil
+  end
+
+  it "should check if the password is correct" do
+    User.create("test_username", "test_password", "test@test.com", "01234567890")
+    expect(User.password.length).to eq 61
   end
 end
