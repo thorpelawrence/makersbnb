5c9665e43444371fd1bb9424471883470ee12e8c
Marc Blenkinsop <marcblenky@hotmail.com>
2019-11-19 11:08:33 +0000

    created listings db migration file

---
+++ db/migrations/listings.sql
@@ -0,0 +1,17 @@
+CREATE TABLE listings
+(
+    PRIMARY KEY,
+    property_name VARCHAR(60) ,
+    home_type VARCHAR(60),
+    area VARCHAR(40),
+    no_beds INT,
+    room_type VARCHAR(40),
+    max_people INT,
+    price MONEY,
+    date_from DATE,
+    date_to DATE,
+    wifi BIT DEFAULT 0,
+    parking BIT DEFAULT 0,
+    kitchen BIT DEFAULT 0,
+    heating BIT DEFAULT 0
+);
