80988c862d4e1742fca0828e826ca34ec3eef087
slongboro <susanlongley@gmail.com>
2019-11-19 15:04:19 +0000

    Added property test and logic, passing

---
+++ db/migrations/listings.sql
@@ -0,0 +1,18 @@
+CREATE TABLE listings
+(
+    id SERIAL PRIMARY KEY,
+    area VARCHAR(60),
+    property_name VARCHAR(60) ,
+    home_type VARCHAR(60),
+    no_beds INT,
+    room_type VARCHAR(40),
+    max_people INT,
+    price MONEY,
+    date_from DATE,
+    date_to DATE,
+    wifi BOOLEAN DEFAULT FALSE,
+    parking BOOLEAN DEFAULT FALSE,
+    kitchen BOOLEAN DEFAULT FALSE,
+    heating BOOLEAN DEFAULT FALSE,
+    image_link VARCHAR
+);
