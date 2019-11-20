d72d852b09383bfd5079c9b80dc806fd91f2586f
Marc Blenkinsop <marcblenky@hotmail.com>
2019-11-20 11:13:52 +0000

    added 'add-space' functionality for db, spelling check on erb views

--- db/migrations/listings.sql
+++ db/migrations/listings.sql
@@ -15,6 +15,7 @@ CREATE TABLE listings
     parking BOOLEAN DEFAULT FALSE,
     kitchen BOOLEAN DEFAULT FALSE,
     heating BOOLEAN DEFAULT FALSE,
-
+    username VARCHAR,
+    FOREIGN KEY (username) REFERENCES users(username),
     image_link VARCHAR
 );
--- lib/property.rb
+++ lib/property.rb
@@ -1,19 +1,18 @@
-require_relative '../database'
+require_relative "../database"
+# require_relative "lib/user"
 
 class Property
-
-    def self.create(home_type, area, room_type, max_people, price,
-    date_from, date_to, property_name, no_beds, wifi, parking, kitchen, heating, image_link)
+  def self.create(home_type, area, room_type, max_people, price,
+                  date_from, date_to, property_name, no_beds, wifi, parking, kitchen, heating, image_link, username)
     connection = Database.connect
     connection.exec_params("INSERT INTO listings (home_type, area, room_type, max_people, price,
-        date_from, date_to, property_name, no_beds, wifi, parking, kitchen, heating, image_link) 
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)", [home_type, area, room_type, max_people, price,
-            date_from, date_to, property_name, no_beds, wifi, parking, kitchen, heating, image_link])
-    end
+        date_from, date_to, property_name, no_beds, wifi, parking, kitchen, heating, image_link, username) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)", [home_type, area, room_type, max_people, price,
+                                                                                     date_from, date_to, property_name, no_beds, wifi, parking, kitchen, heating, image_link, username])
+  end
 
-    def self.all
-        connection = Database.connect
-        connection.exec("SELECT * FROM listings").to_a
-      end
-
+  def self.all
+    connection = Database.connect
+    connection.exec("SELECT * FROM listings").to_a
+  end
 end
