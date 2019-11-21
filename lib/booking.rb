385d6d00f1d910607f795d0af4267aaaffc8dc3f
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-11-21 10:49:11 +0000

    created test and functionality for create method

--- lib/booking.rb
+++ lib/booking.rb
@@ -1,3 +1,14 @@
+require 'pg'
+
 class Booking 
-    
+    def self.create(listing_id, guest_username, date_from, date_to)
+        connection = Database.connect
+        connection.exec_params("INSERT INTO booking (listing_id, guest_username, date_from, date_to) VALUES ($1, $2, $3, $4)",
+                               [listing_id, guest_username, date_from, date_to])
+    end
+
+    def self.get_booking_by_guest_username(username)
+        connection = Database.connect
+        booking = connection.exec_params("SELECT * FROM booking WHERE guest_username=$1", [username]).first
+    end
 end
--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -1,4 +1,6 @@
 require 'booking'
+require 'property'
+require 'user'
 require 'pg'
 
 describe Booking do
@@ -6,5 +8,11 @@ describe Booking do
         expect(subject).to be_an_instance_of(Booking)
     end
 
-    
+    it "should create a booking" do
+        User.create("test_username", "test_password", "a@a.com", "01234567890")
+        new_prop = Property.create("house", "London", "double", "4", "200", "2019-12-01", "2019-12-07", "Buck House",
+                    "2", "TRUE", "TRUE", "TRUE", "TRUE", "NULL", "test_username").first
+        Booking.create(new_prop['id'], "test_username", "2019-12-02", "2019-12-03")
+        expect(Booking.get_booking_by_guest_username("test_username")).not_to be_nil
+      end
 end
